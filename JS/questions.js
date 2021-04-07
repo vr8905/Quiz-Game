0a3d296f77eb5d60412695c6070922b3fbfb98bd
vr8905 <vaibhavrathod8905@gmail.com>
2021-04-07 23:44:00 +0530

    first commit

---
+++ JS/questions.js
@@ -0,0 +1,169 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "National Income estimates in India are prepared by ?",
+    answer: "Central Statistical Organization",
+    options: [
+      "Planning Commission",
+      "Reserve Bank of India",
+      "Central Statistical Organization",
+      "Indian Statistical Institute"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The staple food of the Vedic Aryan was ?",
+    answer: "Milk and its products",
+    options: [
+      "Barley and rice",
+      "Milk and its products",
+      "Rice and pulses",
+      "Vegetables and fruits"
+    ]
+  },
+    {
+    numb: 3,
+    question: "MS-Word is an example of _____ ?",
+    answer: "Application software",
+    options: [
+      "An operating system",
+      "A processing device",
+      "Application software",
+      "An input device"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Fathometer is used to measure ?",
+    answer: "Ocean depth",
+    options: [
+      "Earthquakes",
+      "Rainfall",
+      "Ocean depth",
+      "Sound intensity"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Epsom(England) is the place associated with ?",
+    answer: "Horse racing",
+    options: [
+      "Snooker",
+      "Polo",
+      "Horse racing",
+      "Shooting"
+    ]
+  },
+  {
+    numb: 6,
+    question: "“One People, One State, One leader” was the policy of ?",
+    answer: "Hitler",
+    options: [
+      "Stalin",
+      "Hitler",
+      "Lenin",
+      "Mussolin"
+    ]
+  },
+  {
+    numb: 7,
+    question: "DRDL stands for ?",
+    answer: "Defence Research and Development Laboratory",
+    options: [
+      "Defence Research and Development Laboratory",
+      "Department of Research and Development Laboratory",
+      "Differential Research and Documentation Laboratory",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who is known as 'the father of Indian missile technology' ?",
+    answer: "Dr. A.P.J. Abdul Kalam",
+    options: [
+      "Dr. U.R. Rao",
+      "Dr. A.P.J. Abdul Kalam",
+      "Dr. Chidambaram",
+      "Dr. Homi Bhabha"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which is considered as the biggest port of India ?",
+    answer: "Mumbai",
+    options: [
+      "Chennai",
+      "Kolkata",
+      "Cocin",
+      "Mumbai"
+    ]
+  },
+  {
+    numb: 10,
+    question: "FFC stands for ?",
+    answer: "Film Finance Corporation", 
+    options: [
+      "Foreign Finance Corporation",
+      "Film Finance Corporation",
+      "Federation of Football Council",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Name the plant important in sericulture ?",
+    answer: "Mulberry",
+    options: [
+      "Pea",
+      "Legumes",
+      "Mulberry",
+      "Cassia"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which is a green planet in the solar system ?",
+    answer: "Uranus",
+    options: [
+      "Pluto", 
+      "Venus",
+      "Uranus",
+      "Mars"
+    ]
+  },
+  {
+    numb: 13,
+    question: "The most important small scale industry in india is the ?",
+    answer: "Handloom Industry",
+    options: [
+      "Textile Industry",
+      "Paper Industry",
+      "Handloom Industry",
+      "Jute industry"
+    ]
+  },
+  {
+    numb: 14,
+    question: "An astronaut in outer space will observe sky as ?",
+    answer: "Black",
+    options: [
+      "White",
+      "Black",
+      "Blue",
+      "Red"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which one of the following rivers is recharged by subsoil water ?",
+    answer: "Narmada",
+    options: [
+      "Godavari",
+      "Damodar",
+      "Narmada",
+      "Krishna"
+    ]
+  },
+  
+];
